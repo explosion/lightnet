530c2baf4bd155245dc292b30633983cf6a72a35
Matthew Honnibal <honnibal+gh@gmail.com>
2017-11-19 22:59:35 +0100

    Add data manager

--- lightnet/lightnet.pyx
+++ lightnet/lightnet.pyx
@@ -1,6 +1,7 @@
 # cython: infer_types=True
 from __future__ import print_function
 from libc.stdlib cimport calloc, free, rand
+cimport numpy as np
 import shutil
 import tempfile
 import numpy
@@ -15,30 +16,88 @@ except NameError:
 cdef extern from "_darknet/utils.h" nogil:
     float rand_uniform(float min, float max)
 
-
 cdef extern from "_darknet/image.h" nogil:
     void place_image(image im, int w, int h, int dx, int dy, image canvas)
 
 cdef extern from "_darknet/data.h" nogil:
-    void fill_truth_detection(char *path, int num_boxes, float *truth,
-                              int classes, int flip,
-                              float dx, float dy, float sx, float sy)
+    void correct_boxes(box_label *boxes, int n, float dx, float dy, float sx, float sy, int flip)
+    void randomize_boxes(box_label *b, int n)
 
 
-cdef data load_data_detection(int n, char **paths,
-        int m, int w, int h, int boxes, int classes,
-        float jitter, float hue, float saturation, float exposure) nogil:
-    cdef data d
-    d.shallow = 0
+cdef class Image:
+    cdef image c
 
-    d.X.rows = n
-    d.X.vals = <float**>calloc(d.X.rows, sizeof(float*))
-    d.X.cols = h*w*3
+    def __init__(self, int w, int h, int c): 
+        self.c = make_image(w, h, c)
 
-    d.y = make_matrix(n, 5*boxes)
-    cdef float dw, dh, nw, nh, dx, dy
-    for i in range(n):
-        orig = load_image_color(paths[i], 0, 0)
+    @classmethod
+    def random(cls, int w, int h, int c):
+        cdef Image self = Image.__new__(cls)
+        self.c = make_random_image(w, h, c)
+        return self
+
+    @classmethod
+    def load(cls, path, int w, int h, int c):
+        path = Path(path)
+        if not path.exists():
+            raise IOError("Image not found: %s" % path)
+        cdef Image self = Image.__new__(cls)
+        cdef bytes loc = unicode(path.resolve()).encode('utf8')
+        self.c = load_image(<char*>loc, w, h, c)
+        return self
+    
+    @classmethod
+    def load_color(cls, path, int w=0, int h=0):
+        path = Path(path)
+        if not path.exists():
+            raise IOError("Color image not found: %s" % path)
+        cdef Image self = Image.__new__(cls)
+        cdef bytes loc = unicode(path.resolve()).encode('utf8')
+        self.c = load_image_color(<char*>loc, w, h)
+        return self
+
+    def __dealloc__(self):
+        free_image(self.c)
+
+
+cdef class BoxLabels:
+    cdef box_label* c
+    cdef int n
+
+    def __init__(self, path=None):
+        cdef bytes loc = unicode(Path(path).resolve()).encode('utf8')
+        self.c = read_boxes(loc, &self.n)
+
+    def __dealloc__(self):
+        free(self.c)
+
+
+cdef class DetectionData:
+    cdef data c
+
+    def __init__(self, images, labels, int w, int h, int num_boxes,
+            float jitter=0.2, float hue=0.1, float saturation=1.5, float exposure=1.5):
+        self.c.shallow = 0
+        cdef int n = len(images)
+        self.c.X.rows = n
+        self.c.X.vals = <float**>calloc(self.c.X.rows, sizeof(float*))
+        self.c.X.cols = h*w*3
+        self.c.y = make_matrix(n, 5*num_boxes)
+
+        cdef Image image
+        cdef BoxLabels boxes
+        for i, (image, boxes) in enumerate(zip(images, labels)):
+            self._fill_Xy_vals(&self.c.X.vals[i], self.c.y.vals[i],
+                image.c, boxes.c, num_boxes,
+                w, h, jitter, hue, saturation, exposure)
+ 
+    def __dealloc__(self):
+        free_data(self.c)
+
+    cdef float* _fill_Xy_vals(self, float** X, float* truth,
+            image orig, box_label* boxes, int num_boxes, int w, int h,
+            float jitter, float hue, float saturation, float exposure) nogil:
+        cdef float dw, dh, nw, nh, dx, dy
         sized = make_image(w, h, orig.c)
         fill_image(sized, .5)
 
@@ -65,64 +124,19 @@ cdef data load_data_detection(int n, char **paths,
         flip = rand() % 2
         if flip:
             flip_image(sized)
-        d.X.vals[i] = sized.data
+        X[0] = sized.data
+        randomize_boxes(boxes, num_boxes)
+        correct_boxes(boxes, num_boxes, -dx/w, -dy/h, nw/w, nh/h, flip)
+        for i in range(num_boxes):
+            box = boxes[i]
+            if (box.w < .001 or box.h < .001):
+                continue
+            truth[i*5+0] = box.x
+            truth[i*5+1] = box.y
+            truth[i*5+2] = box.w
+            truth[i*5+3] = box.h
+            truth[i*5+4] = box.id
 
-        fill_truth_detection(paths[i], boxes, d.y.vals[i], classes,
-                             flip, -dx/w, -dy/h, nw/w, nh/h)
-        free_image(orig)
-    return d
-
-
-cdef class Image:
-    cdef image c
-
-    def __init__(self, int w, int h, int c): 
-        self.c = make_image(w, h, c)
-
-    @classmethod
-    def random(cls, int w, int h, int c):
-        cdef Image self = Image.__new__(cls)
-        self.c = make_random_image(w, h, c)
-        return self
-
-    @classmethod
-    def load(cls, path, int w, int h, int c):
-        path = Path(path)
-        if not path.exists():
-            raise IOError("Image not found: %s" % path)
-        cdef Image self = Image.__new__(cls)
-        cdef bytes loc = unicode(path.resolve()).encode('utf8')
-        self.c = load_image(<char*>loc, w, h, c)
-        return self
-    
-    @classmethod
-    def load_color(cls, path, int w, int h):
-        path = Path(path)
-        if not path.exists():
-            raise IOError("Color image not found: %s" % path)
-        cdef Image self = Image.__new__(cls)
-        cdef bytes loc = unicode(path.resolve()).encode('utf8')
-        self.c = load_image_color(<char*>loc, w, h)
-        return self
-
-    def __dealloc__(self):
-        free_image(self.c)
-
-
-cdef class DetectionData:
-    cdef data c
-
-    def __init__(self, Network net):
-        # I *think* the data from the load_args struct here gets taken over
-        # by the data returned by load_data_detection. That's why we don't
-        # have to free it.
-        cdef load_args a = get_base_args(net.c)
-        self.c = load_data_detection(a.n, a.paths,
-            a.m, a.w, a.h, a.num_boxes, a.classes, a.jitter, a.hue,
-            a.saturation, a.exposure)
-
-    def __dealloc__(self):
-        free_data(self.c)
 
 cdef class Metadata:
     cdef metadata c
@@ -161,6 +175,10 @@ cdef class Network:
         if self.c != NULL:
             free_network(self.c)
 
+    @property
+    def num_boxes(self):
+        return num_boxes(self.c)
+
     def load_meta(self, path):
         self.meta = Metadata(path)
 
@@ -185,46 +203,16 @@ cdef class Network:
         self.load_meta(path / 'coco.template')
         return self
 
-    def __call__(self, loc, 
+    def __call__(self, Image image, 
             float thresh=.5, float hier_thresh=.5, float nms=.45):
-        return self.detect(loc, thresh, hier_thresh, nms)
+        return self.detect(image, thresh, hier_thresh, nms)
 
-    #def update(self, images, labels):
-    #    load_args args = {0};
-    #    args.w = net->w;
-    #    args.h = net->h;
-    #    args.paths = paths;
-    #    args.n = imgs;
-    #    args.m = plist->size;
-    #    args.classes = classes;
-    #    args.jitter = jitter;
-    #    args.num_boxes = side;
-    #    args.d = &buffer;
-    #    args.type = REGION_DATA;
-
-    #    args.angle = net->angle;
-    #    args.exposure = net->exposure;
-    #    args.saturation = net->saturation;
-    #    args.hue = net->hue;
-    #    
-    #    args.d = &data
-    #    args.type = REGION_DATA;
-    #    args.angle = net->angle;
-    #    args.exposure = net->exposure;
-    #    args.saturation = net->saturation;
-    #    args.hue = net->hue;
-
-    #    pthread_t load_thread = load_data_in_thread(args);
-    #    data = load_data_detection(len(images), paths, m, w, boxes, classes, jitter, hue, saturation, exposure)
-    #    loss = train_network(self.c, train)
-        
-    def detect(self, loc,
+    def detect(self, Image image,
             float thresh=.5, float hier_thresh=.5, float nms=.45):
-        cdef Image im = Image.load_color(loc, 0, 0)
         cdef box* boxes = make_boxes(self.c)
         cdef float** probs = make_probs(self.c)
         num = num_boxes(self.c)
-        network_detect(self.c, im.c, thresh, hier_thresh, nms, boxes, probs)
+        network_detect(self.c, image.c, thresh, hier_thresh, nms, boxes, probs)
         res = []
         cdef int j, i
         for j in range(num):

de6c29c0b28d3c144b5d55c2f31cf6a0e05554ba
Matthew Honnibal <honnibal+gh@gmail.com>
2017-11-21 20:16:37 +0100

    Add test for Image.from_bytes

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from numpy.testing import assert_equal
+
 from lightnet.lightnet import Image
 
 def test_make_image():
@@ -7,3 +10,12 @@ def test_make_image():
 def test_random_image():
     img = Image.random(10, 10, 10)
     img2 = Image.random(100, 10, 10)
+
+def test_image_from_bytes():
+    path = Path("tests/COCO_val2014_000000000042.jpg")
+    loaded = Image.load_color(path)
+    with path.open('rb') as file_:
+        raw = file_.read()
+    made = Image.from_bytes(raw)
+    assert_equal(made.data, loaded.data)
+

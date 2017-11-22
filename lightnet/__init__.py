1c93b0a2826f43d08bebe69a67e5b87c5ef0f34c
ines <ines@ines.io>
2017-11-22 04:11:47 +0100

    Fix formatting, docstrings and unicode declarations

--- lightnet/__init__.py
+++ lightnet/__init__.py
@@ -1,5 +1,9 @@
+# coding: utf8
+from __future__ import unicode_literals
+
 from .lightnet import Network, Image, BoxLabels
 from .about import __version__
+
 
 def load(name, path=None):
     return Network.load(name, path=path)
